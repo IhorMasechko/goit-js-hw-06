7820e81b9c955895d7a1082638f69cf3034c3dce
IhorMasechko <igor.masechko007@gmail.com>
2022-12-23 20:33:50 +0200

    First push

---
+++ js/task-02.js
@@ -0,0 +1,10 @@
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ulList = document.querySelector('#ingredients');
+
+const list = ingredients.reduce((acc, item) => {
+  return acc + `<li>${item}</li>`;
+}, '');
+
+ulList.innerHTML = list;
+console.log(ulList);
